28219e934abdd3aa021b178e27a0048c672cf7e1
Aman Priyadarshi <aman.eureka@gmail.com>
2015-08-22 02:14:31 +0530

    Made Files php

--- web/index.php
+++ web/index.php
@@ -23,22 +23,7 @@
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <nav class="navbar navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Sceptor</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </nav>
+    <?php include('.\views\nav.php'); ?>
     <div class="header">
         <div class="container intro-body">
             <div class="col-md-4 text-center">
