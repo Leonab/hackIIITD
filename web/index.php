bb384acdb6f12366593dbcc65f013fdca079f992
Manoj Pandey <manojpandey1996@gmail.com>
2015-08-22 03:26:09 +0530

    first layout:

--- web/index.php
+++ web/index.php
@@ -9,28 +9,59 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Sceptor | Cool Stuff</title>
+    <title>jghjg</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" href="img/favicon.png" type="image/icon">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/md-buttons.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 </head>
 
 <body>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <?php include('.\views\nav.php'); ?>
+    
+    <?php include("views/nav.php");?>
+
     <div class="header">
-        <div class="container intro-body">
-            <div class="col-md-4 text-center">
-                Left section
-            </div>
-            <div class="col-md-8 text-center">
-                Right section
+        <div class="container intro text-center">
+        
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container end">
+            <img src="img/tedxmsit-small.png">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <div class="social-links-bottom">
+                <ul class="list-inline">
+                    <li>
+                        <a href="">
+                            <i class="fa fa-facebook fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-twitter fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-youtube fa-2x"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -40,6 +71,7 @@
     </script>
     <script src="js/vendor/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
+    <script src="js/md-buttons.js"></script>
 </body>
 
 </html>
--- web/views/nav.php
+++ web/views/nav.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Sceptor</a>
+        <div class="nav-bar container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                </button>
+                <a class="logo navbar-brand" href="#" title="">
+                    <img src="img/tedxmsit-logo.png">
+                </a>
+            </div>
+            <div id="navbar" class="links navbar-collapse collapse">
+               
+            </div>
+            <!--/.navbar-collapse -->
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-        </div>
-        <!--/.navbar-collapse -->
-    </div>
-</nav>
+    </nav>
